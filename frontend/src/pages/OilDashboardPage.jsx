9ec8b11b4f339028a79aca0fe5aba48fc5e0e97e
nontapat150-droid <nontapat150@gmail.com>
2026-06-12 12:46:09 +0700

    Fix JSX syntax error

--- frontend/src/pages/OilDashboardPage.jsx
+++ frontend/src/pages/OilDashboardPage.jsx
@@ -742,6 +742,7 @@ export default function OilDashboardPage() {
                   })}
                 </div>
               </div>
+              </div>
             )}
 
             {/* History Table */}
