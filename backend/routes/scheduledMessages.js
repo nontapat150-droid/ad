8118ca35c4998de55495fcdd4928374189a2f0dc
nontapat150-droid <nontapat150@gmail.com>
2026-06-26 15:44:41 +0700

    fix: correct db import paths in scheduledMessages and scheduler

--- backend/routes/scheduledMessages.js
+++ backend/routes/scheduledMessages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../config/db');
 const { loadSchedules } = require('../scheduler');
 
 // Get all scheduled messages
--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const db = require('./db');
+const db = require('./config/db');
 const { getMessaging } = require('firebase-admin/messaging');
 
 let activeJobs = {};
